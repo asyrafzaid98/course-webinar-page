7b49db4e2e3d16694200d3e6dddd63c917d14039
asyrafzaid98 <asy.zaid98@gmail.com>
2024-10-08 22:34:22 +0800

    update page

--- src/router/index.ts
+++ src/router/index.ts
@@ -17,6 +17,16 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/community',
+      name: 'community',
+      component: () => import('../views/CommunityView.vue')
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('../views/ContactView.vue')
     }
   ]
 })
